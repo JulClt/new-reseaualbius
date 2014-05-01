9bee9b9b64cbf02ab40788b9df40a333450773f6
Julien <julien.collet01@gmail.com>
2014-05-01 16:06:19 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewReseaualbius::Application.config.secret_key_base = '8dd5bca0fc2228b1ed4a73e86d0a35e0e0e8585df7325b3d2ae14ef3e9b795b8931f634b3b4798f88149963b56744da2bd6865904e622c83695f6a6f930463e5'
